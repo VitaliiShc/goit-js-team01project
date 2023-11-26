309fe77708d39882fb4c69b88005ad163f0f8a6a
VitaliiShc <vitaliy.shchukin@gmail.com>
2023-11-26 05:32:30 +0200

    upd index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import './js/homepage.js';
 import './js/popular.js';
 import './js/discount.js';
 import './js/firstProdGet.js';
-import "./js/pagination-logic.js"
-import './js/pagination.js'
+import './js/pagination-logic.js';
+import './js/pagination.js';
+import './js/popup-cart.js';
