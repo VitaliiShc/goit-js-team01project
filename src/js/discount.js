eba54a2d6206077999482748265f8031451f9f94
Mark <tka4mark@gmail.com>
2023-11-26 20:56:14 +0200

    discount and popular

--- src/js/discount.js
+++ src/js/discount.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const refs = {
-    dicountList: document.querySelector('.discount_list')
+    discountList: document.querySelector('.discount_list')
 }
 
 const baseUrl = "https://food-boutique.b.goit.study/api/products/discount";
@@ -21,20 +21,34 @@ export function createMarkupDiscount(response) {
     const limitedResponse = response.slice(0, 2);
 
     for (let i = 0; i < limitedResponse.length; i++) {
-        const { name, img, price } = limitedResponse[i];
+        const { _id, name, img, price } = limitedResponse[i];
 
-        const markup = `<li class="discount_card">
+        const markup = `<li class="discount_card" data-id="${_id}">
                 <div class="dicount_img_icon">
-                    <img class="discount_photo" src="${img}" alt="No description" loading="lazy" width="114px" height="114px"/>
+                    <img class="discount_photo" src="${img}" alt="No description" loading="lazy" width="114px" height="114px" data-id="${_id}">
                     <span class="discount_icon"></span>
                 </div>
                 <div class="discount_info">
-                    <p class="discount_product_name">${name}</p>
-                    <p class="discount_product_price">${price}</p>
+                    <p class="discount_product_name" data-id="${_id}">${name}</p>
+                    <p class="discount_product_price" data-id="${_id}">${price}</p>
                     <button class=""></button>
                 </div>
         </li>`;
 
-        refs.dicountList.insertAdjacentHTML('beforeend', markup);
+        refs.discountList.insertAdjacentHTML('beforeend', markup);
     }
+}
+
+refs.discountList.addEventListener('click', e => {
+    const id = e.target.dataset.id;
+    if (id) {
+        const product = getProductId(id);
+        console.log(product);
+    }
+});
+
+function getProductId(id) {
+    const storedItems = JSON.parse(localStorage.getItem('discountData')) || [];
+    const find = storedItems.find(obj => obj['_id'] === id);
+    return find;
 }
