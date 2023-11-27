32e169823e3ac93f43044d073f1a11dd48bd9d24
VitaliiShc <vitaliy.shchukin@gmail.com>
2023-11-27 16:45:29 +0200

    add remove Listener addto card btn

--- src/js/addToCart.js
+++ src/js/addToCart.js
@@ -2,16 +2,13 @@ export function addToCart(event) {
   let buyingProd = event.currentTarget;
   let productId = buyingProd.id;
 
-  console.log(event.currentTarget);
-  console.log(event.target);
-  console.log(productId);
-
   const savedProduct1 = JSON.parse(localStorage.getItem('res.data'));
   const prodInCart = savedProduct1.find(option => option._id === productId);
   const currentCart = JSON.parse(localStorage.getItem('cart')) || [];
   const productAlreadyInCart = currentCart.find(
     item => item._id === prodInCart._id
   );
+
   if (!productAlreadyInCart) {
     currentCart.push(prodInCart);
     localStorage.setItem('cart', JSON.stringify(currentCart));
