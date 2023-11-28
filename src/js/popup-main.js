40d9c18817867e91c6ae3e5d065bd4cf22b6f793
VitaliiShc <vitaliy.shchukin@gmail.com>
2023-11-28 19:17:20 +0200

    test in progress

--- src/js/popup-main.js
+++ src/js/popup-main.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
+import icons from '../images/icons.svg';
+import { addToCart } from './addToCart';
 
 const refs = {
   // elements
-  discountList: document.querySelector('.discount_list'),
-  popularList: document.querySelector('.popular_list'),
   body: document.querySelector('body'),
-  productItem: document.querySelector('.prod-item'),
   productList: document.querySelector('.list-prod'),
   addToCartBtn: document.querySelector('.buy-btn'),
 };
@@ -30,38 +29,12 @@ export async function getProductById(id) {
 // * open modal
 
 refs.productList.addEventListener('click', async e => {
-  if (e.target === refs.addToCartBtn) {
-    console.log('Catr');
-  } else if (e.target !== refs.productList) {
+  if (e.target !== refs.productList) {
     console.log(e.target.dataset.id);
     e.preventDefault();
     const id = e.target.dataset.id;
     const data = await getProductById(id);
     renderPopup(data);
-
-
-
-    
-
-    // const popupMain = document.getElementById('popap-main');
-    // const closeBtn = document.querySelector('.popup-main-close-btn');
-
-    // // * close modal by Button - X
-    // closeBtn.addEventListener('click', () => {
-    //   closeModal(popupMain);
-    // });
-    // // * close modal by Dropbox
-    // popupMain.addEventListener('click', e => {
-    //   if (e.target === popupMain) {
-    //     closeModal(popupMain);
-    //   }
-    // });
-    // // * close modal by Escape
-    // window.addEventListener('keydown', e => {
-    //   if (e.code === 'Escape') {
-    //     closeModal(popupMain);
-    //   }
-    // });
   }
 });
 
@@ -69,15 +42,16 @@ refs.productList.addEventListener('click', async e => {
 
 export function renderPopup(data) {
   if (!data) {
-    console.log('No data');
     return;
   }
-  const { category, desc, img, name, price, size, popularity } = data;
+  const { category, desc, img, name, price, size, popularity, _id } = data;
   console.log(data);
   const markup = `<div id="popap-main" class="popup-main">
   <div class="popup-main-content">
     <button class="popup-main-close-btn" type="button">
-      X
+      <svg class="popup-main-close">
+        <use href="${icons}#icon-remove" />
+      </svg>
     </button>
     <div class="popup-main-conteiner">
       <div class="popup-main-wrap">
@@ -102,18 +76,20 @@ export function renderPopup(data) {
     </div>
     <div class="popup-main-footer">
       <p class="popup-main-price">$${price}</p>
-      <button class="popup-main-add-btn" type="button">
-        Add to Cart
+      <button class="popup-main-add-btn" type="button" id=${_id}>
+        Add to <svg class="popup-main-icon">
+        <use href="${icons}#icon-cart" />
+        </svg>
       </button>
     </div>
-  </div>
-</div>
-`;
+    </div>
+  </div>`;
   refs.body.insertAdjacentHTML('afterbegin', markup);
+  document.body.classList.add('no-scroll');
 
   const popupMain = document.getElementById('popap-main');
   const closeBtn = document.querySelector('.popup-main-close-btn');
-  const sddToCartBtn = document.querySelector('.popup-main-add-btn');
+  const addToCartBtn = document.querySelector('.popup-main-add-btn');
 
   // * close modal by Button - X
   closeBtn.addEventListener('click', () => {
@@ -132,35 +108,175 @@ export function renderPopup(data) {
     }
   });
   // * add to cart btl listener
-  // sddToCartBtn.addEventListener('click', addToCart);
-  // додати саме в кошик
-  // змініти текст коннетн на "added to cart"
-  // зняти слухача
-  // закрити модалку
-
+  addToCartBtn.addEventListener('click', addToCart);
 }
 
 // ! close popup functions
 
 function closeModal(popupMain) {
   popupMain.classList.add('is-hidden');
+  document.body.classList.remove('no-scroll');
 }
 
-// add to cart
+// import axios from 'axios';
 
-// function addToCart(event) {
-//   let buyingProd = event.currentTarget;
-//   let productId = buyingProd.id;
-//   console.log(productId);
+// const refs = {
+//   // elements
+//   discountList: document.querySelector('.discount_list'),
+//   popularList: document.querySelector('.popular_list'),
+//   body: document.querySelector('body'),
+//   productItem: document.querySelector('.prod-item'),
+//   productList: document.querySelector('.list-prod'),
+//   addToCartBtn: document.querySelector('.buy-btn'),
+// };
 
-//   const savedProduct1 = JSON.parse(localStorage.getItem('res.data'));
-//   const prodInCart = savedProduct1.find(option => option._id === productId);
-//   const currentCart = JSON.parse(localStorage.getItem('cart')) || [];
-//   const productAlreadyInCart = currentCart.find(
-//     item => item._id === prodInCart._id
-//   );
-//   if (!productAlreadyInCart) {
-//     currentCart.push(prodInCart);
-//     localStorage.setItem('cart', JSON.stringify(currentCart));
+// // * get id of product
+
+// export async function getProductById(id) {
+//   try {
+//     const BASE_API = 'https://food-boutique.b.goit.study/api';
+//     const END_POINT = `/products/${id}`;
+//     const url = `${BASE_API}${END_POINT}`;
+
+//     const response = await axios.get(url);
+//     const data = await response.data;
+//     return data;
+//   } catch (error) {
+//     return;
+//     // console.error(error);
 //   }
 // }
+
+// // * open modal
+
+// refs.productList.addEventListener('click', async e => {
+//   if (e.target === refs.addToCartBtn) {
+//     console.log('Catr');
+//   } else if (e.target !== refs.productList) {
+//     console.log(e.target.dataset.id);
+//     e.preventDefault();
+//     const id = e.target.dataset.id;
+//     const data = await getProductById(id);
+//     renderPopup(data);
+
+//     // const popupMain = document.getElementById('popap-main');
+//     // const closeBtn = document.querySelector('.popup-main-close-btn');
+
+//     // // * close modal by Button - X
+//     // closeBtn.addEventListener('click', () => {
+//     //   closeModal(popupMain);
+//     // });
+//     // // * close modal by Dropbox
+//     // popupMain.addEventListener('click', e => {
+//     //   if (e.target === popupMain) {
+//     //     closeModal(popupMain);
+//     //   }
+//     // });
+//     // // * close modal by Escape
+//     // window.addEventListener('keydown', e => {
+//     //   if (e.code === 'Escape') {
+//     //     closeModal(popupMain);
+//     //   }
+//     // });
+//   }
+// });
+
+// // ! render
+
+// export function renderPopup(data) {
+//   if (!data) {
+//     console.log('No data');
+//     return;
+//   }
+//   const { category, desc, img, name, price, size, popularity } = data;
+//   console.log(data);
+//   const markup = `<div id="popap-main" class="popup-main">
+//   <div class="popup-main-content">
+//     <button class="popup-main-close-btn" type="button">
+//       X
+//     </button>
+//     <div class="popup-main-conteiner">
+//       <div class="popup-main-wrap">
+//         <img class="popup-main-photo" src="${img}" alt="photo" width="160" />
+//       </div>
+//       <div class="popup-main-text-conteiner">
+//         <p class="popup-main-title">${name}</p>
+//         <ul class="popup-main-list">
+//           <li class="popup-main-subtitles">
+//             <span class="popup-subtitles-style">Category: </span>${category}
+//           </li>
+//           <li class="popup-main-subtitles">
+//             <span class="popup-subtitles-style">Size: </span>${size}
+//           </li>
+//           <br />
+//           <li class="popup-main-subtitles">
+//             <span class="popup-subtitles-style">Popularity: </span>${popularity}
+//           </li>
+//         </ul>
+//         <p class="popup-main-text">${desc}</p>
+//       </div>
+//     </div>
+//     <div class="popup-main-footer">
+//       <p class="popup-main-price">$${price}</p>
+//       <button class="popup-main-add-btn" type="button">
+//         Add to Cart
+//       </button>
+//     </div>
+//   </div>
+// </div>
+// `;
+//   refs.body.insertAdjacentHTML('afterbegin', markup);
+
+//   const popupMain = document.getElementById('popap-main');
+//   const closeBtn = document.querySelector('.popup-main-close-btn');
+//   const sddToCartBtn = document.querySelector('.popup-main-add-btn');
+
+//   // * close modal by Button - X
+//   closeBtn.addEventListener('click', () => {
+//     closeModal(popupMain);
+//   });
+//   // * close modal by Dropbox
+//   popupMain.addEventListener('click', e => {
+//     if (e.target === popupMain) {
+//       closeModal(popupMain);
+//     }
+//   });
+//   // * close modal by Escape
+//   window.addEventListener('keydown', e => {
+//     if (e.code === 'Escape') {
+//       closeModal(popupMain);
+//     }
+//   });
+//   // * add to cart btl listener
+//   // sddToCartBtn.addEventListener('click', addToCart);
+//   // додати саме в кошик
+//   // змініти текст коннетн на "added to cart"
+//   // зняти слухача
+//   // закрити модалку
+
+// }
+
+// // ! close popup functions
+
+// function closeModal(popupMain) {
+//   popupMain.classList.add('is-hidden');
+// }
+
+// // add to cart
+
+// // function addToCart(event) {
+// //   let buyingProd = event.currentTarget;
+// //   let productId = buyingProd.id;
+// //   console.log(productId);
+
+// //   const savedProduct1 = JSON.parse(localStorage.getItem('res.data'));
+// //   const prodInCart = savedProduct1.find(option => option._id === productId);
+// //   const currentCart = JSON.parse(localStorage.getItem('cart')) || [];
+// //   const productAlreadyInCart = currentCart.find(
+// //     item => item._id === prodInCart._id
+// //   );
+// //   if (!productAlreadyInCart) {
+// //     currentCart.push(prodInCart);
+// //     localStorage.setItem('cart', JSON.stringify(currentCart));
+// //   }
+// // }
