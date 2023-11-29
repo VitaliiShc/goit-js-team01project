36af0eac0a89dc626900ac9b418dcef2afe22a65
VitaliiShc <vitaliy.shchukin@gmail.com>
2023-11-29 06:55:28 +0200

    fix conflicts

--- src/js/cart.js
+++ src/js/cart.js
@@ -19,21 +19,21 @@ export function renderCartPage(YOUR_CART) {
 
 
 function clearCart(YOUR_CART) {
-  const removeCartItemBtn = document.querySelector('.remove-cart-item-btn');
+//   const removeCartItemBtn = document.querySelector('.remove-cart-item-btn');
 
-  removeCartItemBtn.addEventListener('click', e => {
-    const YOUR_CART = JSON.parse(localStorage.getItem('cart'));
-    const id = e.currentTarget.datast.productid;
+//   removeCartItemBtn.addEventListener('click', e => {
+//     const YOUR_CART = JSON.parse(localStorage.getItem('cart'));
+//     const id = e.currentTarget.datast.productid;
 
-    const getStorageId = YOUR_CART.find(el => el._id === id);
-    console.log(getStorageId);
+//     const getStorageId = YOUR_CART.find(el => el._id === id);
+//     console.log(getStorageId);
 
-    const newCart = YOUR_CART.filter(el => el !== getStorageId);
-    localStorage.setItem('cart', JSON.stringify(newCart));
+//     const newCart = YOUR_CART.filter(el => el !== getStorageId);
+//     localStorage.setItem('cart', JSON.stringify(newCart));
 
-    renderCartPage(YOUR_CART);
-  });
-}
+//     renderCartPage(YOUR_CART);
+//   });
+// }
 
 
 
