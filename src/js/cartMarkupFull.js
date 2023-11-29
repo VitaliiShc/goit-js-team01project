473dd2d93ac6848381f374d1c943c8aa873f82d7
VitaliiShc <vitaliy.shchukin@gmail.com>
2023-11-29 03:58:57 +0200

    cart markup restored

---
+++ src/js/cartMarkupFull.js
@@ -0,0 +1,53 @@
+import icons from '../images/icons.svg';
+import { calcTotalPrice } from './cartTotalPrice';
+
+export function cartMarkupFull(YOUR_CART) {
+  const fullCartMarkup =
+    `<div class="delete-all">
+        <p class="delete-all-text">Delete all</p>
+            <button type="button" class="delete-all-btn">
+                <svg class="delete-all-icon">
+                    <use href="${icons}#icon-remove"></use>
+                </svg>
+            </button>
+    </div>
+    <ul class="your-cart-list">` +
+    YOUR_CART.map(
+      ({ category, img, name, price, size, _id }) =>
+        `<li class="yourcart-product-card" data-product-id=${_id}>
+            <img class="yourcart-product-img" src=${img} alt=${name} />
+            <div class="yourcart-product-card-discription">
+                <div class="yourcart-product-card-info">
+                    <p class="yourcart-product-name">${name}</p>
+                    <div class="yourcart-product-features">
+                        <p class="yourcart-product-category">Category: <span>${category}</span></p>
+                        <p class="yourcart-product-size">Size: <span>${size}</span></p>
+                    </div>
+                    <p class="yourcart-product-price">${price}</p>
+                </div>
+                <div class="yourcart-product-card-controls">
+                    <button type="button" class="delete-btn"><svg class="delete-icon"><use href="${icons}#icon-remove"></use>/svg></button>
+                    <!-- <div class="counter"></div> -->
+                </div>
+            </div>
+        </li>`
+    ).join('') +
+    `</ul>
+    <div class="order">
+        <h2 class="order-title">Your Order</h2>
+        <div class="order-total">
+            <p class="order-total">Total</p>
+            <div class="order-sum">
+                <p class="order-total-sum">
+                    <span class="order-text-sum">Sum: </span>&#36;${calcTotalPrice(
+                      YOUR_CART
+                    )}</p>
+            </div>
+        </div>
+        <form class="form-input">
+            <input type="email" name="user-email" id="user-email" class="mail-input" placeholder="Enter your email" pattern="[A-Za-z0-9._+\-']+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}" required/>
+            <button type="submit" class="form-btn">Checkout</button>
+        </form>
+    </div>`;
+  return fullCartMarkup;
+}
