3dd46f70a7b3cafaf6de79db3d4ec902ee8f112b
TarasKupchenko <133959505+TarasKupchenko@users.noreply.github.com>
2023-11-30 01:37:28 +0200

    change

--- src/js/privacy-modal.js
+++ src/js/privacy-modal.js
@@ -1,12 +1,33 @@
-  function openModal(modalId) {
-    let modal = document.getElementById(modalId);
-    modal.classList.remove("is-hidden");
-    document.body.classList.add('no-scroll');
-
-  }
-
-  function closeModal(modalId) {
-    let modal = document.getElementById(modalId);
-    modal.classList.add("is-hidden");
-    document.body.classList.remove('no-scroll');
-  }
+function openModal(modalId) {
+   let modal = document.getElementById(modalId);
+   modal.classList.remove("is-hidden");
+   document.body.classList.add('no-scroll');
+ 
+   function closeModalHandler(e) {
+     if (e.target === modal) {
+       closeModal(modalId);
+     }
+   }
+ 
+   modal.addEventListener('click', closeModalHandler);
+ 
+   window.addEventListener('keydown', (e) => {
+     if (e.code === 'Escape') {
+       closeModal(modalId);
+     }
+   });
+ }
+ 
+ function closeModal(modalId) {
+   let modal = document.getElementById(modalId);
+   modal.classList.add("is-hidden");
+   document.body.classList.remove('no-scroll');
+   modal.removeEventListener('click', closeModalHandler);
+   
+   window.removeEventListener('keydown', (e) => {
+     if (e.code === 'Escape') {
+       closeModal(modalId);
+     }
+   });
+ }
+ 
