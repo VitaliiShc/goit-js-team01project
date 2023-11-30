4f5cf5a9fbcc4b32cd0a3601bcc30f682bab0acc
VitaliiShc <vitaliy.shchukin@gmail.com>
2023-11-30 14:58:23 +0200

    fix conflicts

--- src/js/privacy-modal.js
+++ src/js/privacy-modal.js
@@ -1,31 +1,30 @@
 function openModal(modalId) {
-   let modal = document.getElementById(modalId);
-   modal.classList.remove("is-hidden");
-   document.body.classList.add('no-scroll');
- 
-   function closeModalHandler(e) {
-     if (e.target === modal) {
-       closeModal(modalId);
-     }
-   }
- 
-   modal.addEventListener('click', closeModalHandler);
- 
-   window.addEventListener('keydown', (e) => {
-     if (e.code === 'Escape') {
-       closeModal(modalId);
-     }
-   });
- }
- 
- function closeModal(modalId) {
-   let modal = document.getElementById(modalId);
-   modal.classList.add("is-hidden");
-   document.body.classList.remove('no-scroll');
-   window.removeEventListener('keydown', (e) => {
-     if (e.code === 'Escape') {
-       closeModal(modalId);
-     }
-   });
- }
- 
+  let modal = document.getElementById(modalId);
+  modal.classList.remove('is-hidden');
+  document.body.classList.add('no-scroll');
+
+  function closeModalHandler(e) {
+    if (e.target === modal) {
+      closeModal(modalId);
+    }
+  }
+
+  modal.addEventListener('click', closeModalHandler);
+
+  window.addEventListener('keydown', e => {
+    if (e.code === 'Escape') {
+      closeModal(modalId);
+    }
+  });
+}
+
+function closeModal(modalId) {
+  let modal = document.getElementById(modalId);
+  modal.classList.add('is-hidden');
+  document.body.classList.remove('no-scroll');
+  window.removeEventListener('keydown', e => {
+    if (e.code === 'Escape') {
+      closeModal(modalId);
+    }
+  });
+}
