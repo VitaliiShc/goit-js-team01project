3228d002a6c626f784c9ad49753558d613371fda
VitaliiShc <vitaliy.shchukin@gmail.com>
2023-11-30 19:56:59 +0200

    fix bags

--- src/js/popup-cart.js
+++ src/js/popup-cart.js
@@ -1,3 +1,11 @@
+import { cartMarkupEmpty } from './cartMarkupEmpty.js';
+import { cartMarkupFull } from './cartMarkupFull.js';
+import { cartClear } from './cartClear.js';
+import { cartQuantityItemsCount } from './cartQuantityItemsCount.js';
+import { cartRemoveItem } from './cartRemoveItem.js';
+import { cartOrderPost } from './cartOrderPost.js';
+import { cartPageRender } from './cart.js';
+
 const refs = {
   checkModalBtn: document.querySelector('.check-btn'),
   popupCart: document.querySelector('.popup-cart'),
@@ -6,7 +14,10 @@ const refs = {
 
 refs.checkModalBtn.addEventListener('click', e => {
   e.preventDefault();
+  localStorage.setItem('cart', '[]');
   refs.popupCart.classList.remove('is-hidden');
+
+  cartPageRender();
   window.addEventListener('keydown', closeByEscape);
 });
 
