d009d33d74ab311be656b6d4daab217b27e9a5ba
klodmone <kladmone54@gmail.com>
2023-11-30 11:06:33 +0100

    Summary

--- src/js/firstProdGet.js
+++ src/js/firstProdGet.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import icons from '../images/icons.svg';
 const axiosFirst = axios.create({
   baseURL: 'https://food-boutique.b.goit.study/api/products',
   params: {
@@ -15,7 +16,7 @@ export class searchFirstAPI {
       // return res.data;
       localStorage.setItem('res.data', JSON.stringify(res.data.results));
       // added - replace function execution
-      createFirst();
+      // createFirst();
       const buyClick = document.querySelectorAll('.buy-btn');
       buyClick.forEach(button => {
         button.addEventListener('click', addToCart);
@@ -55,14 +56,14 @@ function handleResize() {
   const newLimitProd = findLimitProd(newScreenWidth);
   if (newLimitProd !== limitProd) {
     limitProd = newLimitProd;
-    createFirst();
+    // createFirst();
   }
 }
 window.addEventListener('resize', handleResize);
 export function createFirst(currentPage) {
-  console.log(currentPage);
   const savedProduct = localStorage.getItem('res.data');
   const parseItem = JSON.parse(savedProduct);
+  const productKey = JSON.parse(localStorage.getItem('products'));
   const productsList = document.querySelector('.list-prod');
   productsList.innerHTML = '';
   let firstElOnPage;
@@ -73,11 +74,11 @@ export function createFirst(currentPage) {
     firstElOnPage = (page - 1) * limit;
   }
   limitтNumberProd = +pageProd * +limitProd;
-  console.log(firstElOnPage);
-  console.log(limitтNumberProd);
+  // console.log(firstElOnPage);
+  // console.log(limitтNumberProd);
   try {
     const dataItems = parseItem;
-    if (currentPage >= 2) {
+    if (dataItems && currentPage >= 2) {
       let pageCounter = (currentPage - 1) * 8;
       const itemsToDisplay = dataItems.slice(
         firstElOnPage + pageCounter,
@@ -87,7 +88,19 @@ export function createFirst(currentPage) {
         const markup = creatMarkupProd(itemsToDisplay[i]);
         productsList.insertAdjacentHTML('beforeend', markup);
       }
-    } else if (dataItems && dataItems.length > 0) {
+    }
+    // else if (productKey && currentPage >= 2) {
+    //   let pageCounter = (currentPage - 1) * 8;
+    //   const itemsToDisplay = productKey.slice(
+    //     firstElOnPage + pageCounter,
+    //     limitтNumberProd + pageCounter
+    //   );
+    //   for (let i = 0; i < itemsToDisplay.length; i += 1) {
+    //     const markup = creatMarkupProd(itemsToDisplay[i]);
+    //     productsList.insertAdjacentHTML('beforeend', markup);
+    //   }
+    // }
+    else if (dataItems && dataItems.length > 0) {
       const itemsToDisplay = dataItems.slice(firstElOnPage, limitтNumberProd);
       for (let i = 0; i < itemsToDisplay.length; i += 1) {
         const markup = creatMarkupProd(itemsToDisplay[i]);
@@ -101,20 +114,20 @@ export function createFirst(currentPage) {
 export function creatMarkupProd(item) {
   const { _id, category, name, img, price, size, is10PercentOff, popularity } =
     item;
-  const nameWithSpace = name.replace(/_/g, ' ');
-  const categoryWithSpace = category.replace(/_/g, ' ');
+  // const nameWithSpace = name.replace(/_/g, ' ');
+  // const categoryWithSpace = category.replace(/_/g, ' ');
   return `<li class="prod-item" data-id=${_id}>
                 <div class="prod-pic" data-id=${_id}>
                   <svg class="discont-prod" width="60" height="60" style="visibility: ${onVisible(
                     is10PercentOff
                   )};">
-                    <use href="icons.svg#icon-cart"></use>
+                    <use href="${icons}.svg#icon-discount"></use>
                   </svg>
                   <img class="prod-img" src="${img}" alt="${name}" loading="lazy" data-id=${_id} />
                 </div>
-                <h3 class="title-prod" data-id=${_id}>${nameWithSpace}</h3>
+                <h3 class="title-prod" data-id=${_id}>${name}</h3>
                 <div class="feature" data-id=${_id}>
-                  <p class="feature-prod" data-id=${_id}>Category:<span class="feature-value" data-id=${_id}>${categoryWithSpace}</span></p>
+                  <p class="feature-prod" data-id=${_id}>Category:<span class="feature-value" data-id=${_id}>${category}</span></p>
                   <p class="feature-prod" data-id=${_id}>Size:<span class="feature-value" data-id=${_id}>${size}</span></p>
                   <p class="feature-prod push" data-id=${_id}>Popularity:<span class="feature-value" data-id=${_id}>${popularity}</span></p>
                 </div>
@@ -122,7 +135,7 @@ export function creatMarkupProd(item) {
                   <p class="price-prod" data-id=${_id}>$${price}</p>
                   <button class="buy-btn" type="button" id=${_id}>
                       <svg class="buy-svg" width="18" height="18">
-                        <use href="icons.svg#icon-cart"></use>
+                        <use href="${icons}.svg#icon-cart"></use>
                       </svg>
                 </button>
                 </div>
